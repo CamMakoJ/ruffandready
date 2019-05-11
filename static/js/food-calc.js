d7d5862a0321f3f2a2d2044ed0b4ce3e73b867d4
CamMakoJ <cam@makoj.com>
2019-05-11 02:33:29 -0600

    updated calc and added a few tweaks overall

--- static/js/food-calc.js
+++ static/js/food-calc.js
@@ -3,18 +3,45 @@ function calculateFood() {
   var activityLevel = document.getElementById("activityLevel").value;
   var dogsAge = document.getElementById("dogsAge").value;
 
-
-
-  var total = Math.round(((dogWeight * activityLevel) * 1000) * dogsAge);
-
-
+  if (system == "Kg") {
+    //Metric Math
+    var total = Math.round(((dogWeight * activityLevel) * 1000) * dogsAge);
+  } else {
+    //Imperial Math
+    var total = Math.round((((dogWeight * 2.20462) * activityLevel) * 16) * dogsAge);
+    total = total.toFixed(1);
+  }
   document.getElementById("totalFood").style.display = "block";
   document.getElementById("food").innerHTML = total;
+  document.getElementById('units').innerText = units;
 }
 
 document.getElementById("totalFood").style.display = "none";
-
-
 document.getElementById("calculate").onclick = function() {
   calculateFood();
 };
+
+var system = "Kg"
+var systemLabel = "Metric"
+var units = "grams"
+
+function checkSystem() {
+  if (system == "Kg") {
+    system = "lb";
+    systemLabel = "Imperial";
+    units = "oz"
+  } else {
+    system = "Kg";
+    systemLabel = "Metric";
+    units = "grams"
+  }
+  document.getElementById('systemMeasure').innerText = system;
+  document.getElementById('systemLabel').innerText = systemLabel;
+
+  calculateFood();
+  return
+}
+
+
+var toggle = document.getElementById("toggle")
+toggle.addEventListener("click", checkSystem, false);
