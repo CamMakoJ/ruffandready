2281f9538031a32b9b09509164099eff66803932
Cam J <cam@makoj.com>
2021-02-14 14:56:13 -0700

    creating endoresement carousel

---
+++ static/js/slick-activate.js
@@ -0,0 +1,13 @@
+
+  $(document).ready(function() {
+$('.slick-carousel').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 10000,
+  centerMode: false,
+  variableWidth: false,
+  dots: false,
+  adaptiveHeight: true
+});
+  });
